41e8d28370a5c1f43e7725d0f3e04f0e72309494
Cesar Sul <csul@usc.edu>
2021-06-10 07:04:37 -0700

    update project3

--- projects/project2/solution/generate_makeflow.py
+++ projects/project2/solution/generate_makeflow.py
@@ -5,8 +5,8 @@ import math
 def usage():
     print("Usage:")
     print("-h,--help        \tDisplay this message")
-    print("-d,--debug       \tEnable debug mode (process only 2 seconds of song")
-    print("-f,--song_file   \tSong file to read song information from")
+    print("-f,--flac_file   \t.flac file to read song information from")
+    print("-r,--rosa_path   \tPath to rosa_fft.py")
 
 
 
@@ -30,12 +30,14 @@ def main():
         if o in ("-h", "--help"):
             usage()
             sys.exit(0)
-        elif o in ("-f", "--song_file"):
+        elif o in ("-f", "--flac_file"):
             song_file=a
         elif o in ("-o", "--output_dir"):
             output_dir=a
         elif o in ("-s", "--sample_rate"):
             song_sr=int(a)
+        #elif o in ("-r","--rosa_path"):
+        #    rosa_path=a
 
 
 
@@ -48,12 +50,13 @@ def main():
     
     
     movie_file=output_files_dir+"/"+song_name+'.mp4'
-    dummy_file=output_files_dir+"/time_index_{file_id:04d}_complete"
     
     ffmpeg_command1='ffmpeg -threads 4 -framerate 60 -i '+output_files_dir+ '/images/frame%09d.png -i ' +song_file+ ' -pix_fmt yuv420p  ' +movie_file
-    python_command='python3 {script_path} -f {flac_file}  -i {start_time} -o {outdir}'
-    dummy_command='touch ' +dummy_file
-
+    python_command1='python3 {script_path} -f {flac_file}  -i {start_time} -o {outdir}'
+    frameName=output_files_dir+'/images/frame{task_id:09d}.png'
+    dummy_filename=output_files_dir+'/index_{task_id:04d}_complete'
+    dummy_command='touch ' +dummy_filename 
+    
     y,sr=librosa.load(song_file,sr=song_sr)
 
     if debug:
@@ -68,53 +71,40 @@ def main():
     print("Should be %d s and %d frames long"%(max_time,max_frame))
     
     os.makedirs(makeflow_dir,exist_ok='True')
-
-
     with open(makeflow_dir+'/'+song_name+".makeflow", "w") as makefile:
-        
-        # Write portion of makeflow file that describes ffmpeg job
-        # Format is:
-        # outputfile(s) : inputfile(s)
-        # (tab) command to generate outputfile(s)
 
-        # Output file done for you
+        # Merge images and song into .mp4
+        # There are way too many .png files to track in makeflow like this
+        # Instead we will check for index_XXX_complete files and assume the
+        # accompanying images exist
+
         makefile.write(movie_file+": ")
-        # TODO: write list of input files
-        makefile.write(dummy_file.format(file_id=0) + " ")
-        for i in range(10):
-            makefile.write(output_files_dir+"/inputfile_{file_id:04d} ".format(file_id=i) + " ")
-
-        # ffmpeg command done for you
+        for time_index in range(max_time):
+                makefile.write(dummy_filename.format(task_id=(time_index)) + " " )
         makefile.write("\n\t" +  ffmpeg_command1 + "\n\n")
     
+        # Intermediate step to reduce number of input files required for
+        # ffmpeg job
 
-        time_index=0
-        # Write itermediate step to reduce number of input files
-        # required for ffmpeg job
-        #
-        # Format is:
-        # outputfile(s) : inputfile(s)
-        # (tab) command to generate outputfile(s)
+        for time_index in range(max_time):
+            makefile.write(dummy_filename.format(task_id=time_index) + ": ") 
+            for counter in range(0,60):
+                if (time_index*60+counter) >= max_frame :
+                    break
+                makefile.write(frameName.format(task_id=time_index*60+counter) + " ")
+            makefile.write("\n\t"+dummy_command.format(task_id=time_index)+"\n\n") 
 
-        #Output file done for you
-        makefile.write(dummy_file.format(file_id=time_index) + ": ")
-        # TODO: write list of input files
-        for i in range(10):
-            makefile.write(output_files_dir+"/inputfile_{file_id:04d} ".format(file_id=i) + " ")
-        makefile.write("\n\t"+dummy_command.format(file_id=time_index)+"\n\n") 
-    
-        #Write portion of makeflow file that describes rosa_fft.py job
-        # Format is:
-        # outputfile(s) : inputfile(s)
-        # (tab) command to generate outputfile(s)
-    
-        # TODO: write list of outputfiles
-        makefile.write(output_files_dir+"/outputfile_{file_id:04d}".format(file_id=time_index))
-        # Input file done for you
-        makefile.write(": " + song_file)
-        
-        # Command to generate input files done for you (mostly)
-        makefile.write("\n\t" + python_command.format(script_path=rosa_path,flac_file=song_file,start_time=time_index,outdir=output_files_dir) )
-        
+        # Generate images
+        for time_index in range(max_time):
+
+            for counter in range(0,60):
+                if (time_index*60+counter) >= max_frame :
+                    break
+                makefile.write(frameName.format(task_id=time_index*60+counter) + " ")
+
+            makefile.write(": "+ song_file)
+            makefile.write("\n\t"+python_command1.format(script_path=rosa_path,flac_file=song_file,start_time=time_index,outdir=output_files_dir) +"\n\n")
+
+
 if __name__== "__main__":
     main()
