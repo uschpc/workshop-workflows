4f6b04cc6c4e51442432110d4028e7b8ff379377
Cesar Sul <csul@usc.edu>
2021-11-04 17:06:38 -0700

    update script, test changes to job array

--- scripts/rosa_fft.py
+++ scripts/rosa_fft.py
@@ -1,19 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib as mpl
-mpl.use('Agg')
-import librosa, librosa.display
+import matplotlib.animation as animation
+import librosa
 import getopt,sys,os
+from tqdm.auto import tqdm
 
 def usage():
     print("Usage:")
     print("-h,--help        \tDisplay this message")
     print("-f,--flac_file   \t.flac file to read song information from")
-    print("-o,--output_dir  \tDirectory to save output files (output by default)")
-    print("-i,--start_time  \tBeginning of window of interest")
-    print("-t,--duration    \tLength of window of interest in sec (1 by default)")
+    print("-o,--output_dir  \tDirectory to save output file (output by default)")
     print("-s,--sample_rate \tSample rate of .flac song (44100 by default)")
 
+def animate(i,line,power,t):
+    t.update()
+    line.set_ydata(power[i])
+
+#def init(line,power):
+#    line.set_ydata(power[0])
 
 def main():
     song_file=[]
@@ -22,14 +26,14 @@ def main():
     output_dir="output"
     start_time=0
     duration=1 # Default 1s window
-    
+
     try:
-        opts,args = getopt.getopt(sys.argv[1:],"hf:o:i:t:s:",["help","flac_file=","output_dir=","start_time=","duration=","sample_rate="])
+        opts,args = getopt.getopt(sys.argv[1:],"hf:o:s:",["help","flac_file=","output_dir=","sample_rate="])
     except getopt.GetoptError as err:
         print(err)
         usage()
         sys.exit(2)
-    
+
     for o,a in opts:
         if o in ("-h", "--help"):
             usage()
@@ -38,44 +42,42 @@ def main():
             song_file=a
         elif o in ("-o", "--output_dir"):
             output_dir=a
-            os.makedirs(output_dir+"/images", exist_ok=True)
-
-        elif o in ("-i", "--start_time"):
-            start_time=int(a)
-        elif o in ("-t", "--duration"):
-            duration=int(a)
         elif o in ("-s", "--sample_rate"):
             song_sr=int(a)
-    
-    song_data,sr=librosa.load(song_file,sr=song_sr,offset=start_time,duration=duration)
+
+    os.makedirs(output_dir, exist_ok=True)
+    fps=30
+    dpi=150
+
+    song_data,sr=librosa.load(song_file,sr=song_sr,offset=start_time)
     freq=librosa.fft_frequencies(sr=song_sr)
-    S=librosa.stft(song_data,hop_length=int(song_sr/60))
+    S=np.abs(librosa.stft(song_data,hop_length=int(song_sr/fps) ) )
     power=np.abs(S).T
 
+    nFrames,width=power.shape
+
+    plt.style.use("ggplot")
     fig, ax = plt.subplots()
 
 
     ax.set_title(song_file)
-
-    w=len(power[0])//100
-    max_frame=len(power)
-    some_ones=np.ones(w)
-
-    moving_average=np.convolve(power[0],np.ones(w),mode='same')
-    
-    changeable_plot,=ax.semilogx(freq,moving_average,'.')
     ax.set_ylim(0,160)
-    ax.set_xlim(500,2.205*10**4)
-    fig.savefig(output_dir+"/images/frame%09d.png"%(start_time*60),dpi=400,bbox_inches='tight')
+    ax.set_xlim(100,2.205*10**4)
 
-    for counter in range(1,max_frame):
+    line, = ax.semilogx(freq,power[0])
+    #w=len(power[0])//100
+    #max_frame=len(power)
+    #some_ones=np.ones(w)
 
-        moving_average=np.convolve(power[counter],some_ones,mode='same')
+    frames=np.arange(nFrames)
+    t=tqdm(frames,unit='frames')
+    ani_f = animation.FuncAnimation(
+        fig,animate,frames=frames,fargs=(line,power,t))
+    plt.close()
 
-        minp=np.min(moving_average)
-        maxp=np.max(moving_average)
-        changeable_plot.set_data(freq,moving_average)
-        fig.savefig(output_dir+"/images/frame%09d.png"%(counter+start_time*60),dpi=300,bbox_inches='tight')
+    extra_args=[]
+    ani_f.save(output_dir+"/silent.mp4",dpi=dpi,fps=fps,extra_args=extra_args)
+
 
 if __name__== "__main__":
-    main() 
+    main()
